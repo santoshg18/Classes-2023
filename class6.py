65566730bf54506ef5e29182616f58346816ab28
santoshg18 <santosh.gunda18@gmail.com>
2023-02-02 07:26:44 +0530

    Class number 6 files

---
+++ class6.py
@@ -0,0 +1,12 @@
+# Comment
+def display(names):
+    # TODO: Write description for this method.
+    for name in names:
+        # pass
+        print('The student name is:', name)
+        print('The student name is: ' + name)
+
+
+ # list
+students = ['Ram', 'Somu', 'Bheem']
+display(students)
