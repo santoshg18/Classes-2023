405116042bac0553b0af3b7c88ec7a6e45b2370a
Indu512 <indupriya.ed@gmail.com>
2023-02-02 20:42:08 -0600

    task 1

---
+++ Indu/task1_212023.py
@@ -0,0 +1,14 @@
+X = {1,2,3,9,10,11}
+Y = {5,6,10,11}
+Z = {5,6,10,4}
+A = [1,2,8,7]
+print (Y & Z)
+print (X.intersection(Y))
+print (set(X).intersection(Y))
+
+
+
+A = {1,2,3,9,10,11}
+B = [1,2,8,7,]
+output = A.intersection(B)
+print (output)
