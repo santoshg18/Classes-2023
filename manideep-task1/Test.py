7893458e55079bcf3ef260394904307faa84a67d
manideepgunti2307 <manideep.gunti2307@gmail.com>
2023-02-03 07:52:13 +0530

    task-1

---
+++ manideep-task1/Test.py
@@ -0,0 +1,20 @@
+def my_test():
+  list1 = [10, 20, 30, 40]
+  print(list1)
+  list1.append(50)
+  print(list1)
+  list1.remove(20)
+  print(list1)
+  list1.insert(0, 7.77)
+  print(list1)
+  list1.extend(['Dallas', 'irving', 'frisco'])
+  print(list1)
+  list2 = ['school', 'college', 'university']
+  len(list2)
+  print(list2)
+  list2.pop(2)
+  print(list2)
+  list2.reverse()
+  print(list2)
+
+my_test()
