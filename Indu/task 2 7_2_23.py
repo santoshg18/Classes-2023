d633121dc695e6c9cccf8fdf1d12ab36fcac9b14
Indu512 <indupriya.ed@gmail.com>
2023-02-07 21:55:37 -0600

    task 2

---
+++ Indu/task 2 7_2_23.py
@@ -0,0 +1,23 @@
+class printletters:
+    def list1 = ["Santhosh", "Kumar"]
+   print('list'"\n")
+printletters()
+
+
+
+def printletters():
+    list1 = ['Santhosh','kumar']
+    print("\n".join(list1))
+printletters()    
+
+
+
+
+
+class printletters:
+    def list1 = ['Santhosh','kumar']
+    A = len(list1)
+    #printletters(list1)
+    print("\n")
+printletters(list1)
+
